a29f41175470ee844bec729bdd1155ecca79bc28
YJC <openopentw@gmail.com>
2018-01-01 23:10:23 +0800

    After analysis, finally complete Final Program

--- final/test.m
+++ final/test.m
@@ -10,28 +10,38 @@ Close = [pastData.Close];
 Open = [pastData.Open];
 AdjOpen = Open(2:len) + AdjClose(1:len-1) - Close(1:len-1);
 
-% run
-money = 1000;
-hold = 0;
-days = 365;
-for i = 0:days
-    j = days-i;
-    testData = pastData(1:len-j-1);
-    action = myStrategy(testData);
-    if hold == 0 && action == 1
-        %'buy:  ' + string(pastData(len-j).Date)
-        hold = money / AdjOpen(len-j);
-        money = 0;
-    elseif hold ~= 0 && action == -1
-        %'sell: ' + string(pastData(len-j).Date)
-        money = hold * AdjOpen(len-j);
-        hold = 0;
+filters = [0.95 0.99 0.995 1 1.001 1.005 1.01 1.05 1.1];
+for filter_id = 1:length(filters)
+    filter = filters(filter_id);
+    for M = 3:7
+        for n = 10:15
+            N = n*2;
+
+            % run
+            money = 1000;
+            hold = 0;
+            days = 100;
+            for i = 0:days
+                j = days-i;
+                testData = pastData(1:len-j-1);
+                action = testmyStrategy(testData, M, N, filter);
+                if hold == 0 && action == 1
+                    %'buy:  ' + string(pastData(len-j).Date)
+                    hold = money / AdjOpen(len-j);
+                    money = 0;
+                elseif hold ~= 0 && action == -1
+                    %'sell: ' + string(pastData(len-j).Date)
+                    money = hold * AdjOpen(len-1-j);
+                    hold = 0;
+                end
+            end
+
+            if hold ~= 0
+                money = hold * AdjOpen(len-1);
+                hold = 0;
+            end
+
+            disp("(" + string(filter) + ", " + string(M) + ", " + string(N) + ") = " + string(money));
+        end
     end
 end
-
-if hold ~= 0
-    money = hold * AdjOpen(len-1);
-    hold = 0;
-end
-
-money
--- final/testmyStrategy.m
+++ final/testmyStrategy.m
@@ -1,14 +1,13 @@
-function action=myStrategy(pastData)
+function action=testmyStrategy(pastData, M, N, filter)
     % action: 1 for buy, -1 for sell, 0 for nothing
     %% read file
     %file='SPY.csv';
     %spyTable = readtable(file);
     %pastData = table2struct(spyTable);
     %% parameters
-    % M, N: SMA days
-    M = 4;
-    N = 25;
-    filter = 0.99;
+    % N, M: SMA days
+    %N = 20;
+    %M = 5;
     %% algorithm
     % initial
     len = length(pastData);
@@ -18,9 +17,11 @@ function action=myStrategy(pastData)
     Close = [pastData.Close];
     Open = [pastData.Open];
     AdjOpen = Open(2:N+1) + AdjClose(1:N) - Close(1:N);
-    AdjOpenSMA_M = mean(AdjOpen(N-M:N));
-    AdjOpenSMA_N = mean(AdjOpen(1:N));
-    if AdjOpenSMA_M > AdjOpenSMA_N * filter
+    AdjOpenSMA_5 = mean(AdjOpen(N-M:N));
+    AdjOpenSMA_20 = mean(AdjOpen(1:N));
+    if AdjOpenSMA_5 > AdjOpenSMA_20 * filter
+    %if AdjOpenSMA_5 > AdjOpenSMA_20 * 0.999
+    %if AdjOpenSMA_5 > AdjOpenSMA_20
         action = 1;
     else
         action = -1;
